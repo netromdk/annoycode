4650f3db53548a3828018a71cc9041e64545911e
Morten Kristensen <msk@nullpointer.dk>
2015-05-31 21:09:15 +0200

    ac: Better stats.

--- annoycode.py
+++ annoycode.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
         print("Use trainer.py to generate data for use.")
         exit(-1)
 
-    string = "ABC!-"
+    string = "ABC"
     stringEnc = string.encode("utf-8")
     (newString, subs) = data.subMatches(string)
     newStringEnc = newString.encode("utf-8")
 
     inCnt = len(stringEnc)
     outCnt = len(newStringEnc)
-    incPerc = float(outCnt) / float(inCnt) * 100
+    incPerc = float(outCnt) / float(inCnt) * 100 - 100
 
     print("Input:  ", string)
     print("        ", stringEnc)
     print("Output: ", newString)
     print("        ", newStringEnc)
-    print("{} substitutions".format(subs))
-    print("{} -> {} bytes, +{}%".format(inCnt, outCnt, incPerc))
+    print("{} substitutions, {} -> {} bytes, +{}%".\
+          format(subs, inCnt, outCnt, incPerc))
