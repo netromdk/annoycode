aa6dc94219a55bdf35f6f877b256fdc2bc319e46
Morten Kristensen <msk@nullpointer.dk>
2015-06-03 22:57:21 +0200

    Converting to C++ project instead.

--- constants.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-DEFAULT_DATA_FILE = "data.p"
-
-# The initial character will be "!" because below this the values are not
-# graphically representable.
-INIT_SYMBOL_OFFSET = ord("!")
